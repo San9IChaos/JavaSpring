dbd4b42c70497c8bbf0ee555e789b372f5890330
San9IChaos <sav_sas@mail.ru>
2022-09-04 17:39:50 +0300

    Spring course

---
+++ SpringMVC_course3/src/main/java/ru/savelev/spring/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package ru.savelev.spring.dao;
+
+import ru.savelev.spring.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
---
+++ SpringMVC_course3/src/main/java/ru/savelev/spring/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package ru.savelev.spring.service;
+
+import ru.savelev.spring.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
