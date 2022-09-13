6b70176032c70b057253a3bda4cc693b4e1cef50
San9IChaos <sav_sas@mail.ru>
2022-09-13 15:45:13 +0300

    Spring security practise

---
+++ SpringMVC_course5/src/main/java/ru/savelev/spring/configuration/MySecurityInitializer.java
@@ -0,0 +1,5 @@
+package ru.savelev.spring.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {}
