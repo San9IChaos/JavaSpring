f305b49f4c8586b331502e5bd5df7fee27373b26
San9IChaos <sav_sas@mail.ru>
2022-09-19 15:10:49 +0300

    rest api

--- SpringBoot_course1/src/main/java/ru/savelev/springboot/springboot_course1/dao/EmployeeDaoImpl.java
+++ SpringBoot_course1/src/main/java/ru/savelev/springboot/springboot_course1/dao/EmployeeDaoImpl.java
@@ -1,13 +1,12 @@
 package ru.savelev.springboot.springboot_course1.dao;
 
-import org.hibernate.Session;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.savelev.springboot.springboot_course1.entity.Employee;
 
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -19,36 +18,45 @@ public class EmployeeDaoImpl implements EmployeeDAO {
 
     @Override
     public List<Employee> getAllEmployees() {
-        Session session = entityManager.unwrap(Session.class);
+//Поменяли функционал Hibernate на javax.persistence
 
-        List<Employee> allEmployees = session.createQuery("from Employee", Employee.class).getResultList();
+//        Session session = entityManager.unwrap(Session.class);
+//        List<Employee> allEmployees = session.createQuery("from Employee", Employee.class).getResultList();
+
+        Query query = entityManager.createQuery("from Employee");
+        List<Employee> allEmployees = query.getResultList();
 
         return allEmployees;
     }
 
-//    @Override
-//    public void saveEmployee(Employee employee) {
-//        Session session = sessionFactory.getCurrentSession();
-//
+    @Override
+    public void saveEmployee(Employee employee) {
+//        Session session = entityManager.unwrap(Session.class);;
 //        session.saveOrUpdate(employee);
-//
-//    }
-//
-//    @Override
-//    public Employee getEmployee(int id) {
-//        Session session = sessionFactory.getCurrentSession();
-//
+        Employee newEmployee = entityManager.merge(employee);
+        employee.setId(newEmployee.getId());
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+//        Session session = entityManager.unwrap(Session.class);
 //        Employee employee = session.get(Employee.class, id);
-//        return employee;
-//    }
-//
-//    @Override
-//    public void deleteEmployee(int id) {
-//        Session session = sessionFactory.getCurrentSession();
+
+        Employee employee = entityManager.find(Employee.class, id);
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+//        Session session = entityManager.unwrap(Session.class);
 //
 //        Query<Employee> query = session.createQuery("delete from Employee " +
 //                "where id=:employeeId");
 //        query.setParameter("employeeId", id);
 //        query.executeUpdate();
-//    }
+        Query query = entityManager.createQuery("delete from Employee " +
+                "where id=:employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
+    }
 }
