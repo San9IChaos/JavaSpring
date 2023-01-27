015d0ef5dcd72ba37cc9dcb84793064b725bf8fd
San9IChaos <sav_sas@mail.ru>
2023-01-27 15:23:05 +0300

    My practise in February

---
+++ mySpringPractise_2/src/main/java/ru/savelev/spring/TestSpring.java
@@ -0,0 +1,20 @@
+package ru.savelev.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+        System.out.println(comparison);
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+        context.close();
+    }
+}
