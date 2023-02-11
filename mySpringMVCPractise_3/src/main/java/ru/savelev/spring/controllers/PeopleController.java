c983e96a6befc2c2e66d214893f89211cc2bb761
San9IChaos <sav_sas@mail.ru>
2023-02-11 18:53:24 +0300

    Rework connect with DataBase

--- mySpringMVCPractise_3/src/main/java/ru/savelev/spring/controllers/PeopleController.java
+++ mySpringMVCPractise_3/src/main/java/ru/savelev/spring/controllers/PeopleController.java
@@ -22,7 +22,7 @@ public class PeopleController {
     }
 
     @GetMapping()
-    public String index(Model model) throws SQLException {
+    public String index(Model model) {
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
