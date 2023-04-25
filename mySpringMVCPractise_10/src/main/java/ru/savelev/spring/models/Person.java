4d4c5537a3cd034bf90e2d531b14795a1de59ed6
San9IChaos <sav_sas@mail.ru>
2023-04-25 13:38:14 +0300

    JPA practise

---
+++ mySpringMVCPractise_10/src/main/java/ru/savelev/spring/models/Person.java
@@ -0,0 +1,102 @@
+package ru.savelev.spring.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Entity
+@Table(name = "Person")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2,max = 30, message = "Name should be between 2 and 30 characters")
+    private String name;
+
+    @Column(name = "age")
+    @Min(value = 0, message = "Age should be greater than 0")
+    private int age;
+
+    @Column(name = "email")
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @Column(name = "date_of_birth")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "dd/MM/yyyy") // день/месяц/год
+    private Date dateOfBirth;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    public Person() {}
+
+    public Person(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
---
+++ mySpringMVCPractise_12/src/main/java/ru/savelev/spring/dao/PersonDAO.java
@@ -0,0 +1,37 @@
+package ru.savelev.spring.dao;
+
+import jakarta.persistence.EntityManager;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import ru.savelev.spring.models.Person;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Component
+public class PersonDAO {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public PersonDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+    @Transactional(readOnly = true)
+    public void testNPlus1() {
+        Session session = entityManager.unwrap(Session.class);
+
+        Set<Person> people = new HashSet<Person>(session.createQuery("select p from Person p LEFT JOIN FETCH p.items")
+                .getResultList());
+
+        for(Person person : people){
+            System.out.println("Person " + person.getName() + " has: " + person.getItems());
+        }
+
+    }
+}
